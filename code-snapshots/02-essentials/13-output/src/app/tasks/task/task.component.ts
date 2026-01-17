9575e6bc194c49986af28e0ce24803aa79f20774
MDrumm90 <michal.drummond@gmail.com>
2026-01-17 12:06:24 +0100

    feat: refactor module structure; create SharedModule and TasksModule, and move CardComponent to SharedModule

--- code-snapshots/02-essentials/13-output/src/app/tasks/task/task.component.ts
+++ code-snapshots/02-essentials/13-output/src/app/tasks/task/task.component.ts
@@ -1,12 +1,9 @@
 import { Component, Input } from '@angular/core';
-import { DatePipe } from '@angular/common';
 import { Task } from './task.model';
-import { CardComponent } from "../../shared/card/card.component";
 import { TasksService } from '../tasks.service';
 @Component({
   selector: 'app-task',
   standalone: false,
-  imports: [DatePipe, CardComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
